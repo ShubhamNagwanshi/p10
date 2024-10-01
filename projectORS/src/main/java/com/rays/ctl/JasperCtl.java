00888d2498d18e827d962634f053501ae529fe21
ShubhamNagwanshi <142484423+ShubhamNagwanshi@users.noreply.github.com>
2024-10-01 16:39:52 +0530

    Update JasperCtl.java

--- projectORS/src/main/java/com/rays/ctl/JasperCtl.java
+++ projectORS/src/main/java/com/rays/ctl/JasperCtl.java
@@ -1,4 +1,4 @@
-
+/**
 	
 package com.rays.ctl;
 
@@ -56,12 +56,12 @@ import net.sf.jasperreports.engine.JasperReport;
 
 @RestController
 @RequestMapping(value = "/Jasper")
-public class JasperCtl extends BaseCtl<MarksheetForm, MarksheetDTO, MarksheetServiceInt> {
+//public class JasperCtl extends BaseCtl<MarksheetForm, MarksheetDTO, MarksheetServiceInt> {
 
 	/** The session factory. */
 
 //	  @Autowired 
-	private SessionFactory sessionFactory = null;
+	//private SessionFactory sessionFactory = null;
 
 	/** The context. */
 
@@ -69,7 +69,7 @@ public class JasperCtl extends BaseCtl<MarksheetForm, MarksheetDTO, MarksheetSer
 	ServletContext context;
 
 	@PersistenceContext
-	protected EntityManager entityManager;
+	//protected EntityManager entityManager;
 
 	/**
 	 * Display.
@@ -81,28 +81,29 @@ public class JasperCtl extends BaseCtl<MarksheetForm, MarksheetDTO, MarksheetSer
 	 * @throws IOException  Signals that an I/O exception has occurred.
 	 */
 	@GetMapping(value = "/report", produces = { MediaType.APPLICATION_JSON_VALUE })
-	public void display(HttpServletRequest request, HttpServletResponse response)
-			throws JRException, SQLException, IOException {
+//	public void display(HttpServletRequest request, HttpServletResponse response)
+		//	throws JRException, SQLException, IOException {
 		System.out.println("********************** Jasper Ctl*********************");
-		ORSResponse res = new ORSResponse(true);
-		ResourceBundle rb = ResourceBundle.getBundle("application");
+		//ORSResponse res = new ORSResponse(true);
+	//	ResourceBundle rb = ResourceBundle.getBundle("application");
 		//String path = context.getRealPath("C:\\Users\\dell\\JaspersoftWorkspace\\MyReports\\ORS10.jrxml");
-		 String path = context.getRealPath(rb.getString("jasper"));
+	//	 String path = context.getRealPath(rb.getString("jasper"));
 
-		Connection con = null;
-		JasperReport jasperReport = JasperCompileManager.compileReport(rb.getString("jasper"));
-		Map<String, Object> map = new HashMap<String, Object>();
-		this.sessionFactory = entityManager.getEntityManagerFactory().unwrap(SessionFactory.class);
-		con = sessionFactory.getSessionFactoryOptions().getServiceRegistry().getService(ConnectionProvider.class)
-				.getConnection();
-		JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, map, con);
-		byte[] pdf = JasperExportManager.exportReportToPdf(jasperPrint);
-		response.setContentType("application/pdf");
-		response.getOutputStream().write(pdf);
-		response.getOutputStream().flush();
-		System.out.println("Thanks");
+	//	Connection con = null;
+	//	JasperReport jasperReport = JasperCompileManager.compileReport(rb.getString("jasper"));
+	//	Map<String, Object> map = new HashMap<String, Object>();
+	//	this.sessionFactory = entityManager.getEntityManagerFactory().unwrap(SessionFactory.class);
+	//	con = sessionFactory.getSessionFactoryOptions().getServiceRegistry().getService(ConnectionProvider.class)
+	//			.getConnection();
+	//	JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, map, con);
+	//	byte[] pdf = JasperExportManager.exportReportToPdf(jasperPrint);
+	//	response.setContentType("application/pdf");
+	//	response.getOutputStream().write(pdf);
+	//	response.getOutputStream().flush();
+	//	System.out.println("Thanks");
 		// return MediaType.APPLICATION_JSON_VALUE;
-	}
+//	}
 
-}
+// }
 
+ */
