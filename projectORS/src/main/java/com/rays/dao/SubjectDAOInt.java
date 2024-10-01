305027a7cf296f3c757862b2424a4fc12367eae3
ShubhamNagwanshi <shubhamnagwanshi767@gmail.com>
2024-10-01 14:33:12 +0530

    done

---
+++ projectORS/src/main/java/com/rays/dao/SubjectDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.SubjectDTO;
+/**
+ * Nikhil Khandait 
+ *
+ */
+public interface SubjectDAOInt extends BaseDAOInt<SubjectDTO>{
+
+}
